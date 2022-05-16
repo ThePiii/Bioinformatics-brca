40cda5fcb8b238aeec1675de4bb579400c752fae
ThePiii <yf13166387172@gmail.com>
2022-05-16 11:20:44 +0800

    download data

--- getData.R
+++ getData.R
@@ -4,7 +4,7 @@
 library(cgdsr)
 
 # Create CGDS object
-mycgds = CGDS("http://www.cbioportal.org/")
+mycgds <- CGDS("http://www.cbioportal.org/")
 
 # Test the CGDS endpoint URL using a few simple API tests
 test(mycgds) 
@@ -13,14 +13,23 @@ test(mycgds)
 all_TCGA_studies <- getCancerStudies(mycgds)
 
 # Get available case lists (collection of samples) for a given cancer study  
-mycancerstudy = getCancerStudies(mycgds)[50,1]
-mycaselist = getCaseLists(mycgds,mycancerstudy)[8,1]
+mycancerstudy <- getCancerStudies(mycgds)[50,1]
+mycaselist <- getCaseLists(mycgds,mycancerstudy)[8,1]
 
 # Get available genetic profiles
-mygeneticprofile = getGeneticProfiles(mycgds,mycancerstudy)[14,1]
+profiles <- getGeneticProfiles(mycgds,mycancerstudy)
+mRNAid <-  getGeneticProfiles(mycgds,mycancerstudy)[14,1]
+CNAid <- getGeneticProfiles(mycgds,mycancerstudy)[7,1]
 
 # Get data slices for a specified list of genes, genetic profile and case list
-data <- getProfileData(mycgds,c('BRCA1', 'BRCA2'),mygeneticprofile,mycaselist)
+mRNA <- getProfileData(mycgds,c('BRCA1', 'BRCA2'),mRNAid,mycaselist)
+CNA <- getProfileData(mycgds,c('BRCA1', 'BRCA2'),CNAid,mycaselist)
 
 # Get clinical data for the case list
-clinicaldata = getClinicalData(mycgds,mycaselist)
+clinicaldata <- getClinicalData(mycgds,mycaselist)
+
+# save data
+library(writexl)
+write_xlsx(mRNA, 'data/mRNA.xlsx')
+write_xlsx(CNA, 'data/CNA.xlsx')
+write_xlsx(clinicaldata, 'data/clinical.xlsx')
