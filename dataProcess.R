550f6d9b694ba11a6fdb5e56b4e5217d7e444ea4
ThePiii <yf13166387172@gmail.com>
2022-06-15 11:32:46 +0800

    VAR

--- dataProcess.R
+++ dataProcess.R
@@ -30,20 +30,11 @@ new.mRNA <- subset(mRNA,select=apply(mRNA, 2, function(x) !all(is.na(x))))
 sum(is.na(new.mRNA)) # 没有了，但是没想到这个时候mRNA的列数比CNA要少...
 
 
-# PCA
-CNA.pca <- PCA(new.CNA[,2:length(new.CNA)], ncp=500, graph=F)
-CNA.var <- get_pca_var(CNA.pca)
-res <- CNA.var$cor
-head(CNA.var$cos2)
-res <- CNA.pca$ind
-View(res$dist)
+# 计算方差降维
+CNA.var <- apply(new.CNA[,2:length(new.CNA)], 2, var)
+CNA.reduction <- new.CNA %>%
+  select(order(-CNA.var[1:500]))  # order默认升序，需要加个负号
 
-
-CNA.pca <- prcomp(new.CNA[,2:length(new.CNA)], center=T, scale.=T)
-eigs <- (CNA.pca$sdev)^2  # 各成分的方差
-var_cum <- cumsum(eigs)/sum(eigs) # 方差比例
-num_pc <- which.max(var_cum >= var_cum[500])
-CNA.pcs <- CNA.pca$x[, 1:num_pc] # 降维后CNA 
-
-mRNA.pca <- prcomp(new.mRNA[,2:length(new.mRNA)], center=T, scale.=T)
-mRNA.pcs <- mRNA.pca$x[, 1:500] # 降维后mRNA
+mRNA.var <- apply(new.mRNA[,2:length(new.mRNA)], 2, var)
+mRNA.reduction <- new.mRNA %>%
+  select(order(-mRNA.var)[1:500])
