979ffb474ee5115a2e7a9a2526f893fe143220af
ThePiii <yf13166387172@gmail.com>
2022-06-19 19:14:29 +0800

    Final

--- mCCA.R
+++ mCCA.R
@@ -13,3 +13,14 @@ mRNA.cano.covar <- out$ws[[2]][out$ws[[2]] != 0.0]
 
 mRNA.cano <- rbind(gene=mRNA.nonzero,covar=mRNA.cano.covar)
 mRNA.cano
+
+
+for (i in 1:dim(mRNA.cano)[2]){
+  if (mRNA.cano[1,i] %in% CNA.cano[1,]){
+    print(paste0(mRNA.cano[1,i], ":", mRNA.cano[2,i]))
+  }
+}
+
+
+write.csv(t(CNA.cano), "CNA.cano.csv")
+write.csv(t(mRNA.cano), "mRNA.cano.csv")
